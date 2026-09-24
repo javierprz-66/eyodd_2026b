9abe38ac66300785f416ccb498a521a957fbac3a
javierprz-66 <javier7tfuw@gmail.com>
2026-09-24 17:00:01 +0000

    ⭐s01t0401 for

--- s02/s01t0401_for.py
+++ s02/s01t0401_for.py
@@ -5,32 +5,26 @@ Por Ejemplo su n = 100, el programa
 calculara la suma del 1 al 100
 """
 
- # Importar biblioteca time
-import time 
+import time
 
-#Tomnado el tiempo inicial
-timestamp_01 = time.time()
-
-# Programa que calcula la suma 
-# de los "n" numeros naturales
-n = 1500
-total_sum = 0
-
-# Ciclo for
-for number in range(1,n+1):
-    #print(number, end= ",")
-    total_sum = total_sum + number
-    # 1: sum - 0 + 1
-    # sum = 1
-    # 2: sum - 1 +2
-    #sum
-
-
-
-print(f"La suma de 1 hasta {n} es: {total_sum}")
-
-# Tomando el tiempo final
-timestamp_02 = time.time()
-
-# Impresion del tiempo de ejecucion 
-print(f"Tiempo de ejcucion: {(timestamp_02-timestamp_01) * 1e6:.2f} μs")
+# Repetimos 11 veces (del 1 al 11)
+for i in range(1, 12):
+    n = i * 500
+    
+    # 1. Tiempo inicial
+    timestamp_01 = time.time()
+    
+    # 2. Sumamos los números
+    total_sum = 0
+    for numero in range(1, n + 1):
+        total_sum = total_sum + numero
+        
+    # 3. Tiempo final
+    timestamp_02 = time.time()
+    
+    # 4. Calculamos y redondeamos a 2 decimales
+    tiempo = round((timestamp_02 - timestamp_01) * 1000000, 2)
+    
+    # Mostramos el resultado
+    print("n =", n, "| Suma =", total_sum, "| Tiempo =", tiempo, "us")
+ 
